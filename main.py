6fa5df1d383e4a2273597340c1b9f8499464308e
ChaseGreene05 <Cjgnl736@gmail.com>
2021-08-18 11:45:56 -0230

    fixed for turn in

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 
+
 x = [1,2,3,4,5]
-y = [1,2,3,4,5]
+y = [math.sin(1),math.sin(2),math.sin(3),math.sin(4),math.sin(5)]
+
 
 plt.plot(x, y, color = "red", marker = "x")
-plt.xlabel("Time (m)")
-plt.ylabel("Speed (m/s)")
-plt.title("Graph Title")
+plt.title("The sum of 1 to 5")
 plt.show()
